5ee07850a18de349a4ecb5268a485f89ffe51955
emily40830 <emily40830@gmail.com>
2020-08-29 10:44:28 +0800

    feat:set up index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const home = require('./modules/home')
-const record = require('./modules/record')
+const express = require('express');
+const router = express.Router();
+
+const home = require('./modules/home');
+const record = require('./modules/record');
+const users = require('./modules/users');
+const auth = require('./modules/auth');
+const { authenticator } = require('../middleware/auth');
 // const filter = require('./modules/filter')
 
 // router.use('/filter', filter)
-router.use('/record', record)
-router.use('/', home)
+router.use('/record', authenticator, record);
+router.use('/users', users);
+router.use('/auth', auth);
 
+router.use('/', authenticator, home);
 
-module.exports = router
+module.exports = router;
