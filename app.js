4fc0d2d12537ae153b64fb87363c6c3af8e9e527
emily40830 <emily40830@gmail.com>
2020-08-29 10:35:03 +0800

    feat:update app.js for auth

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const routes = require('./routes');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 require('handlebars-helpers')();
 require('./config/mongoose');
+const userPassport = require('./config/passport');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //使用者每次發送request時，要不要去更新session
+    saveUninitialized: true, //把未初始化的session初始化並建立
+  }),
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine(
@@ -26,6 +44,20 @@ app.engine(
 app.use(express.static('public'));
 app.set('view engine', 'handlebars');
 app.use(methodOverride('_method'));
+
+userPassport(app);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.warning_msg = req.flash('warning_msg');
+  res.locals.login_error_msg = req.flash('login_err_msg');
+  res.locals.create_err = req.flash('create_err');
+  return next();
+});
+
 app.use(routes);
 
 app.listen(PORT, () => {
