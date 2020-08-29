f20f03c88985b86cc3fddd0427ae44877cfc2bc0
emily40830 <emily40830@gmail.com>
2020-08-29 22:13:33 +0800

    feat:modify the parameters for render

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -24,9 +24,10 @@ const recordsByUser = (userId, category_id, targetdate) => {
   if (category_id) condition.category_id = category_id;
   if (targetdate)
     condition.date = { $gte: `${targetdate}-1`, $lte: `${targetdate}-31` };
-  console.log(condition);
+  //console.log(condition);
   return new Promise((resolve, reject) => {
     RecordsModel.find(condition)
+      .sort([['date', -1]])
       .lean()
       .then((records) => {
         //console.log(records);
@@ -64,19 +65,56 @@ router.get('/', async (req, res) => {
   const selectDate = req.query.year_month;
   const user_id = req.user._id;
 
-  let recordsByuser = await recordsByUser(user_id, cid, selectDate);
-  let categories = await categoryData(cid);
+  let allRecordsByUser = await recordsByUser(user_id);
+  let sumOfamount = 0; //該使用者總金額
+  allRecordsByUser.forEach((e) => {
+    sumOfamount += e.amount;
+  });
 
+  // 篩選後的結果
+  let recordsByuser = await recordsByUser(user_id, cid, selectDate);
+  let categories = await categoryData();
   console.log(recordsByuser);
   console.log(categories);
 
+  //let sumOfcount = recordsByUser.length; //該使用者總records數
+  let amountBycastegoryDate = 0; // 分類後的金額
+  //console.log(recordsByuser);
+  console.log(recordsByuser.length);
+  //console.log(totalCount);
+  //console.log(categories);
+  //recordsByUser = sortBydate(recordsByUser);
+
   recordsByuser.forEach((ele) => {
+    amountBycastegoryDate += ele.amount;
+    ele.date = getFormatDate(ele.date, 'y-m-d');
     ele.categoryInfo = categories.find(
       (c) => Number(c.category_id) === Number(ele.category_id),
     );
   });
 
-  console.log(recordsByuser);
+  //recordsByUser = recordsByUser.
+  let percent = Math.round((amountBycastegoryDate / sumOfamount) * 100, 1);
+
+  res.render('index', {
+    sumOfamount: amountBycastegoryDate,
+    sumOfcount: recordsByuser.length,
+    records: recordsByuser,
+    categories,
+    percent,
+    selectDate,
+    maxYm: getFormatDate(new Date(), 'y-m'),
+  });
+  // 要傳入handlebars的資料
+
+  // sumOfamount: 0,
+  //             records,
+  //             categories,
+  //             percent: 0,
+  //             selectDate,
+  //             maxYm: getFormatDate(new Date(), 'y-m')
+
+  //console.log(recordsByuser);
 
   //recordsByUser.foreach(r=>)
 
@@ -84,168 +122,168 @@ router.get('/', async (req, res) => {
   //console.log(selectDate);
   // join all data
   //
-  const totalAmount = RecordsModel.aggregate([
-    {
-      $group: {
-        _id: null,
-        totalAmount: { $sum: '$amount' },
-      },
-    },
-  ]).exec();
+  // const totalAmount = RecordsModel.aggregate([
+  //   {
+  //     $group: {
+  //       _id: null,
+  //       totalAmount: { $sum: '$amount' },
+  //     },
+  //   },
+  // ]).exec();
 
-  if (cid) {
-    const categories = CategoryModel.aggregate([
-      {
-        $addFields: {
-          isSelected: { $eq: ['$category_id', Number(cid)] },
-        },
-      },
-    ]).exec();
-    const sumOfamount = RecordsModel.aggregate([
-      {
-        $match: { category_id: Number(cid) },
-      },
-      {
-        $group: {
-          _id: null,
-          sumOfamount: { $sum: '$amount' },
-          count: { $sum: 1 },
-        },
-      },
-    ]).exec();
+  // if (cid) {
+  //   const categories = CategoryModel.aggregate([
+  //     {
+  //       $addFields: {
+  //         isSelected: { $eq: ['$category_id', Number(cid)] },
+  //       },
+  //     },
+  //   ]).exec();
+  //   const sumOfamount = RecordsModel.aggregate([
+  //     {
+  //       $match: { category_id: Number(cid) },
+  //     },
+  //     {
+  //       $group: {
+  //         _id: null,
+  //         sumOfamount: { $sum: '$amount' },
+  //         count: { $sum: 1 },
+  //       },
+  //     },
+  //   ]).exec();
 
-    const records = RecordsModel.aggregate([
-      {
-        $match: { category_id: Number(cid) },
-      },
-      {
-        $lookup: {
-          from: 'categories',
-          localField: 'category_id',
-          foreignField: 'category_id',
-          as: 'categoryInfo',
-        },
-      },
-      {
-        $unwind: '$categoryInfo',
-      },
-      {
-        $sort: { date: -1 },
-      },
-      {
-        $project: {
-          name: 1,
-          amount: 1,
-          categoryInfo: 1,
-          date: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
-        },
-      },
-    ]).exec();
+  //   const records = RecordsModel.aggregate([
+  //     {
+  //       $match: { category_id: Number(cid) },
+  //     },
+  //     {
+  //       $lookup: {
+  //         from: 'categories',
+  //         localField: 'category_id',
+  //         foreignField: 'category_id',
+  //         as: 'categoryInfo',
+  //       },
+  //     },
+  //     {
+  //       $unwind: '$categoryInfo',
+  //     },
+  //     {
+  //       $sort: { date: -1 },
+  //     },
+  //     {
+  //       $project: {
+  //         name: 1,
+  //         amount: 1,
+  //         categoryInfo: 1,
+  //         date: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
+  //       },
+  //     },
+  //   ]).exec();
 
-    Promise.all([sumOfamount, records, categories, totalAmount])
-      // .then(records => res.send(records[0].categoryInfo.categoryIcon))
-      .then(
-        ([sumOfamount, records, categories, totalAmount]) => {
-          if (sumOfamount.length) {
-            const percent = Math.floor(
-              (sumOfamount[0].sumOfamount / totalAmount[0].totalAmount) * 100,
-            );
-            res.render('index', {
-              sumOfamount: sumOfamount[0].sumOfamount,
-              sumOfcount: sumOfamount[0].count,
-              records,
-              categories,
-              percent,
-              selectDate,
-              maxYm: getFormatDate(new Date(), 'y-m'),
-            });
-            // console.log(percent)
-          } else {
-            res.render('index', {
-              sumOfamount: 0,
-              records,
-              categories,
-              percent: 0,
-              selectDate,
-              maxYm: getFormatDate(new Date(), 'y-m'),
-            });
-          }
-        },
-        // console.log(sumOfamount, cid)
-      )
-      // .then([sumOfamount,records] => res.render('index', { records }))
-      .catch((err) => console.log(err));
-  } else {
-    const categories = CategoryModel.find().lean().exec();
-    const sumOfamount = RecordsModel.aggregate([
-      {
-        $group: {
-          _id: null,
-          sumOfamount: { $sum: '$amount' },
-          count: { $sum: 1 },
-        },
-      },
-    ]).exec();
+  //   Promise.all([sumOfamount, records, categories, totalAmount])
+  //     // .then(records => res.send(records[0].categoryInfo.categoryIcon))
+  //     .then(
+  //       ([sumOfamount, records, categories, totalAmount]) => {
+  //         if (sumOfamount.length) {
+  //           const percent = Math.floor(
+  //             (sumOfamount[0].sumOfamount / totalAmount[0].totalAmount) * 100,
+  //           );
+  //           res.render('index', {
+  //             sumOfamount: sumOfamount[0].sumOfamount,
+  //             sumOfcount: sumOfamount[0].count,
+  //             records,
+  //             categories,
+  //             percent,
+  //             selectDate,
+  //             maxYm: getFormatDate(new Date(), 'y-m'),
+  //           });
+  //           // console.log(percent)
+  //         } else {
+  //           res.render('index', {
+  //             sumOfamount: 0,
+  //             records,
+  //             categories,
+  //             percent: 0,
+  //             selectDate,
+  //             maxYm: getFormatDate(new Date(), 'y-m'),
+  //           });
+  //         }
+  //       },
+  //       // console.log(sumOfamount, cid)
+  //     )
+  //     // .then([sumOfamount,records] => res.render('index', { records }))
+  //     .catch((err) => console.log(err));
+  // } else {
+  //   const categories = CategoryModel.find().lean().exec();
+  //   const sumOfamount = RecordsModel.aggregate([
+  //     {
+  //       $group: {
+  //         _id: null,
+  //         sumOfamount: { $sum: '$amount' },
+  //         count: { $sum: 1 },
+  //       },
+  //     },
+  //   ]).exec();
 
-    const records = RecordsModel.aggregate([
-      {
-        $lookup: {
-          from: 'categories',
-          localField: 'category_id',
-          foreignField: 'category_id',
-          as: 'categoryInfo',
-        },
-      },
-      {
-        $unwind: '$categoryInfo',
-      },
-      {
-        $sort: { date: -1 },
-      },
-      {
-        $project: {
-          name: 1,
-          amount: 1,
-          categoryInfo: 1,
-          date: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
-        },
-      },
-    ]).exec();
+  //   const records = RecordsModel.aggregate([
+  //     {
+  //       $lookup: {
+  //         from: 'categories',
+  //         localField: 'category_id',
+  //         foreignField: 'category_id',
+  //         as: 'categoryInfo',
+  //       },
+  //     },
+  //     {
+  //       $unwind: '$categoryInfo',
+  //     },
+  //     {
+  //       $sort: { date: -1 },
+  //     },
+  //     {
+  //       $project: {
+  //         name: 1,
+  //         amount: 1,
+  //         categoryInfo: 1,
+  //         date: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
+  //       },
+  //     },
+  //   ]).exec();
 
-    Promise.all([sumOfamount, records, categories, totalAmount])
-      // .then(records => res.send(records[0].categoryInfo.categoryIcon))
-      .then(([sumOfamount, records, categories, totalAmount]) => {
-        // if (totalAmount) {
-        const totalexistedOrnot = totalAmount.length > 0;
-        const sumexistedOrnot = totalAmount.length > 0;
+  //   Promise.all([sumOfamount, records, categories, totalAmount])
+  //     // .then(records => res.send(records[0].categoryInfo.categoryIcon))
+  //     .then(([sumOfamount, records, categories, totalAmount]) => {
+  //       // if (totalAmount) {
+  //       const totalexistedOrnot = totalAmount.length > 0;
+  //       const sumexistedOrnot = totalAmount.length > 0;
 
-        const sum = totalexistedOrnot ? totalAmount[0].totalAmount : 0;
-        const count = sumexistedOrnot ? sumOfamount[0].count || 1 : 0;
-        //console.log(categories, selectDate);
-        //console.log(selectDate);
-        res.render('index', {
-          sumOfamount: sum,
-          sumOfcount: count,
-          records,
-          categories,
-          percent: 100,
-          selectDate,
-          maxYm: getFormatDate(new Date(), 'y-m'),
-        });
-        // } else {
-        //   res.render('index', {
-        //     sumOfamount: 0,
-        //     sumOfcount: 0,
-        //     records, categories, percent: 0
+  //       const sum = totalexistedOrnot ? totalAmount[0].totalAmount : 0;
+  //       const count = sumexistedOrnot ? sumOfamount[0].count || 1 : 0;
+  //       //console.log(categories, selectDate);
+  //       //console.log(selectDate);
+  //       res.render('index', {
+  //         sumOfamount: sum,
+  //         sumOfcount: count,
+  //         records,
+  //         categories,
+  //         percent: 100,
+  //         selectDate,
+  //         maxYm: getFormatDate(new Date(), 'y-m'),
+  //       });
+  //       // } else {
+  //       //   res.render('index', {
+  //       //     sumOfamount: 0,
+  //       //     sumOfcount: 0,
+  //       //     records, categories, percent: 0
 
-        //   })
-        // }
+  //       //   })
+  //       // }
 
-        //console.log(totalAmount)
-      })
-      // .then([sumOfamount,records] => res.render('index', { records }))
-      .catch((err) => console.log(err));
-  }
+  //       //console.log(totalAmount)
+  //     })
+  //     // .then([sumOfamount,records] => res.render('index', { records }))
+  //     .catch((err) => console.log(err));
+  // }
 });
 
 router.get('/create', (req, res) => {
@@ -292,4 +330,10 @@ const getFormatDate = (date, format) => {
     case 'y-m':
       return year + '-' + month;
   }
+};
+
+const sortBydate = (myArguments) => {
+  return myArguments.sort(function (a, b) {
+    return b.date - a.date;
+  });
 };
