95f50c969dc93ec6a982664318941fc5877a58cc
emily40830 <emily40830@gmail.com>
2020-08-30 11:28:26 +0800

    fix:replace list group to table at index.hbs

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -22,58 +22,9 @@ router.get('/:id', async (req, res) => {
   categories.forEach((c) => {
     c.isSelected = Number(cid) === Number(c.category_id);
   });
-
   const today = getFormatDate(new Date(), 'y-m-d');
 
   res.render('edit', { record: each, categories, today });
-
-  // let cid = 0;
-  // RecordsModel.findById(id)
-  //   .select({ _id: 0, category_id: 1 })
-  //   .then((e) => {
-  //     cid = e.category_id;
-  //     // console.log(e)
-  //     CategoryModel.aggregate([
-  //       {
-  //         $addFields: {
-  //           isSelected: { $eq: ['$category_id', cid] },
-  //         },
-  //       },
-  //     ]).then((categories) => {
-  //       RecordsModel.aggregate([
-  //         {
-  //           $match: {
-  //             _id: obid,
-  //           },
-  //         },
-  //         {
-  //           $project: {
-  //             name: 1,
-  //             amount: 1,
-  //             category_id: 1,
-  //             merchandise: 1,
-  //             date: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
-  //           },
-  //         },
-  //       ]).then((record) => {
-  //         console.log(record);
-  //         const currentDate = new Date();
-  //         let day = ('0' + currentDate.getDate()).slice(-2);
-  //         let month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
-  //         let year = currentDate.getFullYear();
-  //         const today = year + '-' + month + '-' + day;
-  //         res.render('edit', { record: record[0], categories, today });
-  //         // res.send(record.name)
-  //         // console.log(record[0].date)
-  //       });
-  //     });
-  //   })
-  //   .catch((err) => console.log(err));
-
-  // return RecordsModel.findById(id)
-  //   .lean()
-  //   .then(record => res.render('edit', { record, categories }))
-  //   .catch(error => console.log(error))
 });
 
 router.put('/:id/edit', (req, res) => {
@@ -84,6 +35,7 @@ router.put('/:id/edit', (req, res) => {
       (record.name = req.body.name),
         (record.date = req.body.date),
         (record.category_id = Number(req.body.Category)),
+        (record.merchandise = req.body.merchandise),
         (record.amount = Number(req.body.amount));
 
       return record.save();
